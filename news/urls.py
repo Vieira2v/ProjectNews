9f315b96102ae1330b24d6b87963afa33ebfc99b
Bruno Vieira <brunovws77@gmail.com>
2024-04-10 20:24:09 -0300

    Base templates, news-detail page and factory created.

--- news/urls.py
+++ news/urls.py
@@ -2,8 +2,9 @@ from django.urls import path  # type: ignore # noqa: F401
 from django.http import HttpResponse  # type: ignore # noqa: F401
 from . import views
 
+app_name = "news"
 
 urlpatterns = [
-    path('', views.home),
-    path('new/<int:id>/', views.new),
+    path('', views.home, name="home"),
+    path('new/<int:id>/', views.new, name="detail"),
 ]
--- news/views.py
+++ news/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render  # type: ignore # noqa: F401
-from django.http import HttpResponse  # type: ignore # noqa: F401
+from utils.news.factory import make_news
 
 
 def home(request):
-    return render(request, 'news/pages/home.html')
+    return render(request, 'news/pages/home.html', context={
+        'news': [make_news() for _ in range(10)],
+    })
 
 
 def new(request, id):
-    return render(request, 'news/pages/news-view.html')
+    return render(request, 'news/pages/news-view.html', context={
+        'new': make_news(),
+    })
