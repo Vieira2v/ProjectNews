30486984895fb3e195515f00d5082d8ddf45e036
Bruno Vieira <brunovws77@gmail.com>
2024-05-07 20:20:04 -0300

    Some changes to the form and improvements.

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,23 @@
 from django import forms  # type: ignore # noqa: F401
 from django.contrib.auth.models import User  # type: ignore # noqa: F401
+from django.core.exceptions import ValidationError  # type: ignore # noqa: F401
 
 
 class RegisterForm(forms.ModelForm):
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Your password'
+        })
+    )
+    confirm_password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password'
+        })
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,3 +27,33 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+
+        labels = {
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'username': 'Username',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+            }
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        # Aqui estou chamando a classe pai do metodo clean, para
+        # fazer a validação das minhas 2 senhas.
+
+        password = cleaned_data.get('password')
+        # Aqui estou acessando o valor digitado nas 2 password
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise ValidationError({
+                'password': 'Password and Confirm password must be equal',
+                'confirm_password':
+                    'Password and Confirm password must be equal'
+            })
--- authors/views.py
+++ authors/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render  # type: ignore # noqa: F401
+from django.shortcuts import render, redirect  # type: ignore # noqa: F401
 from .forms import RegisterForm
 from django.http import Http404  # type: ignore # noqa: F401
 
 
 def register_view(request):
-    form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    # Aqui estou indicando que os dados estao sendo enviados nesta sessao.
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
@@ -14,7 +16,8 @@ def register_create(request):
     if not request.POST:
         raise Http404()
 
-    form = RegisterForm(request.POST)
-    return render(request, 'authors/pages/register_view.html', {
-        'form': form,
-    })
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
