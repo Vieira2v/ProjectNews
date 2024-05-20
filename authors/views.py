297ea8c647f17de8a2b8c6887e6a1d7071ee5d6c
Bruno Vieira <brunovws77@gmail.com>
2024-05-20 19:03:59 -0300

    Creating dashboard.

--- authors/views.py
+++ authors/views.py
@@ -52,7 +52,6 @@ def login_create(request):
         raise Http404()
 
     form = LoginForm(request.POST)
-    login_url = reverse('authors:login')
 
     if form.is_valid():
         authenticated_user = authenticate(
@@ -68,7 +67,7 @@ def login_create(request):
     else:
         messages.error(request, 'Invalid username or password.')
 
-    return redirect(login_url)
+    return redirect(reverse('authors:dashboard'))
 
 
 @login_required(login_url='authors:login', redirect_field_name='next')
@@ -76,3 +75,8 @@ def logout_view(request):
     logout(request)
     messages.success(request, 'Logout realized with success!')
     return redirect('authors:login')
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def dashboard(request):
+    return render(request, 'authors/pages/dashboard.html')
